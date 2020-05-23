fc5377e6b154013e2f21c1ad339f9ccce277b42d
Leonard Esere <leonardesere@gmail.com>
2020-05-23 13:51:15 -0500

    Added some lines of code, html, and css.

--- script.js/script.js
+++ script.js/script.js
@@ -1,15 +1,33 @@
-function makeRow(hour, currHour){
-    let newDiv = document.createElement("div");
-    newDiv.className = "row";
+// We need to link to Geo-location 
 
-    let timeDiv = makeTimeDiv(hour);
-    newDiv.appendChild(timeDiv);
+    // CODE for GEo_Location API/whatever goes here 
+     // **** 
 
-    let textArea = makeTextArea(hour,currHour);
-    newDiv.appendChild(textArea);
 
-    let saveButton = makeSaveButton(hour);
-    newDiv.appendChild(saveButton);
+// We input a zipcode/address location 
 
-    return newDiv;
-}
+    // CODE for (JS) input section { Google search }
+    // ****
+
+
+//  ( how do we add the zipcode and link it the page) 
+    // (location is witing US)
+
+
+
+// we need be able to click submit and have the submit button linked to the <input> sections of the html and return result
+    // No Idea, google how to link the submit button to the code to make it work well and return required info.
+    // **** 
+
+    
+// This Submit button will pull updated information from the Location DATABASE
+
+// Then, we will  need to use this information to extract query using  JSON to output some exact info (i.e like in the IMOB example)
+
+
+// Local Storage will be needed to  store output info down 
+// to the button of the page. 
+
+
+
+

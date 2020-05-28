90b8fd2e07667f2bc6ac1bb9fff02f86ff46052b
Jesus Garza <jesuscgarza33@gmail.com>
2020-05-28 18:36:22 -0500

    :

--- script.js/script.js
+++ script.js/script.js
@@ -1,15 +1,14 @@
-function makeRow(hour, currHour){
-    let newDiv = document.createElement("div");
-    newDiv.className = "row";
-
-    let timeDiv = makeTimeDiv(hour);
-    newDiv.appendChild(timeDiv);
-
-    let textArea = makeTextArea(hour,currHour);
-    newDiv.appendChild(textArea);
-
-    let saveButton = makeSaveButton(hour);
-    newDiv.appendChild(saveButton);
-
-    return newDiv;
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://covid19-monitor-pro.p.rapidapi.com/coronavirus/cases_by_country_paginated.php?page=%3Crequired%3E&country=United%20States&limit=%3Crequired%3E",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "covid19-monitor-pro.p.rapidapi.com",
+		"x-rapidapi-key": "b545bfc749msh27ea8a628f657c2p142142jsn1c819a1e9bc1"
+	}
 }
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});

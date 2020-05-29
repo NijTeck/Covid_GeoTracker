a9863ed2a3c7776e8ba7e9a19c76d5932d8fc367
Anthony Villegas <villegasanthony87@yahoo.com>
2020-05-28 20:48:36 -0500

    more updates

--- script.js/script.js
+++ script.js/script.js
@@ -1,28 +1,18 @@
-var covidURL = "https://api.covid19api.com/countries" + " " + "PMAK-5ecdc71bcf51720034c0e77e-dcfabbd6b156f6387dca1b42a883fff43a";
+function country(){
+var covidURL = "https://api.covid19api.com/countries" + country + "&appid=PMAK-5ecdc71bcf51720034c0e77e-dcfabbd6b156f6387dca1b42a883fff43a";
 console.log(covidURL);
-
 return $.ajax({
     url: chicagoURL,
     method: "GET"
 
-
 })
 
-//.then(function(response) {
-var mapContainer = $("<div class = 'map-container'>")
-    //console.log("response");
-var results = response.data;
-console.log(response);
-var country = response.name;
 
-});
-/*
-$("#submit").on("click", function(event) {
-    event.preventDefault();
-    console.log("click");
-    //$(".united-states").html("");
-    // $(".map-container").show();
-
-
-})
-*/
+function onButtonClick(button){
+    if(!button) {
+        return;
+    };
+}
+function onSearch (){
+    let
+}

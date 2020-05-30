53c57d780f222b2bcd14e1f1fdc21381b303a4c8
Regeditted <36388273+NijTeck@users.noreply.github.com>
2020-05-30 15:30:56 -0500

    Did some onClick, and CallBacks
    So there was a few changes made to the code, 
    added some call back functions,  and onclick update on both the .html and the script file.

--- script.js/script.js
+++ script.js/script.js
@@ -1,8 +1,69 @@
 
 
-  $(document).ready(function() {
-    $.getJSON("https://api.covid19api.com/summary", function (data) {
-      console.log(data);
-  });
+  $("#search-button").click(CovidApi);
 
-  });
+function CovidApi () {
+  var Slug = $("#Slug").value;
+  var apiURL= "https://api.covid19api.com//dayone/country/"+Slug;
+    $.getJSON(apiURL, function (data) {
+      console.log(data);
+       Country = [];
+      let totalDeath = [];
+          totalConfirmed = [];
+      let totalRecovered = [];
+           console.log(data);
+
+   
+      /*var total_Country = "";
+      var total_Deaths = "";
+      var total_Confirmed = "";
+      var total_Recovered = "";
+
+
+      total_Country = data.Countries[0].Country;
+      total_Death = data.Countries[0].totalDeaths;
+      total_Confirmed = data.Countries[0].totalConfirmed;
+      total_Recovered = data.Countries[0].totalRecovered;*/
+
+
+      // calling a particular country 
+    
+
+    $.each(data.Countries, function(id, obj) {
+      
+       if(obj.Slug === Slug){
+
+                                                       console.log(obj.TotalDeaths);
+
+        Country.push(obj.Countries);
+        totalDeath.push(obj.TotalDeaths);
+        totalConfirmed.push(obj.TotalConfirmed);
+        totalRecovered.push(obj.TotalRecovered);
+        
+      
+      
+
+        totalConfirmed.push(obj.total_Confirmed);
+        //totalDeaths.push(obj.TotalDeaths);
+      
+
+        Country.push(obj.Countries);
+       
+       } 
+    })
+
+    });
+  }
+
+
+
+  /*Country: "United States of America"
+CountryCode: "US"
+Date: "2020-05-30T18:20:41Z"
+NewConfirmed: 24266
+NewDeaths: 1193
+NewRecovered: 6455
+Slug: "united-states"
+TotalConfirmed: 1746019
+TotalDeaths: 102809
+TotalRecovered: 406446 */

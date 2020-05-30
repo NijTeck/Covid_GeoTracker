e265990ef89e2798645f2eefb93b9045cd10a703
Regeditted <36388273+NijTeck@users.noreply.github.com>
2020-05-30 13:11:41 -0500

    got the first API to fire

--- script.js/script.js
+++ script.js/script.js
@@ -1,35 +1,8 @@
-function country(countryInput) {
-	if (countryInput == Dominica){
-		return; 
-	}
-var covidURL = "https://api.covid19api.com/countries" + "Dominica" + "&appid=PMAK-5ecdc71bcf51720034c0e77e-dcfabbd6b156f6387dca1b42a883fff43a";
-console.log(covidURL);
- $.ajax({
-    url: covidURL,
-    method: "GET"
-  })
-  .then(function(response) {
-	  var countryDiv = $("<div class = 'map-container'>")
-	
-	  var results = response.data; 
 
-	  console.log(response);
 
-	  var countryInput = response.name;
+  $(document).ready(function() {
+    $.getJSON("https://api.covid19api.com/summary", function (data) {
+      console.log(data);
   });
-}
-//$("#search-button").click(country);
-//console.log("info");
-var anything = document.getElementById("search-button");
-console.log(anything);
-document.getElementById("search-button").addEventListener("click", myFunction);
-function myFunction() {
-    console.log("info")
-  document.getElementById("search-button").innerHTML = "YOU CLICKED ME!";
-}
 
-function onSearch (){
-    let queryCity = document.getElementById("input").value; 
-    console.info("HERE");
-}
-console.log("info");
+  });

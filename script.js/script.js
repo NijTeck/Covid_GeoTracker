e6fe4c34e561bcfe8cbac037c133df26eee84b59
Anthony Villegas <villegasanthony87@yahoo.com>
2020-05-31 15:10:01 -0500

    adjusted API cal to now receive information & referenced css cdn

--- script.js/script.js
+++ script.js/script.js
@@ -1,47 +1,12 @@
-<<<<<<< HEAD
-function country(countryInput) {
-	if (countryInput == UnitedStates){
-		return; 
-	}
-var covidURL = "https://api.covid19api.com/countries" + "" + "&appid=f877968bf7mshff9af34bbc2a8bap11856djsn5fe6819edb21";
-console.log(covidURL);
- $.ajax({
-    url: covidURL,
-    method: "GET"
-  })
-  .then(function(response) {
-	  var countryDiv = $("<div class = 'map-container'>")
-	
-	  var results = response.data; 
-
-	  console.log(response);
-
-	  var countryInput = response.name;
-  });
-}
-//$("#search-button").click(country);
-//console.log("info");
-var anything = document.getElementById("search-button");
-console.log(anything);
-document.getElementById("search-button").addEventListener("click", myFunction);
-function myFunction() {
-    console.log("info")
-  document.getElementById("search-button").innerHTML = "YOU CLICKED ME!";
-}
-
-function onSearch (){
-    let queryCity = document.getElementById("input").value; 
-    console.info("HERE");
-}
-
-=======
 
 
   $("#search-button").click(CovidApi);
 
 function CovidApi () {
-  var Slug = $("#Slug").value;
-  var apiURL= "https://api.covid19api.com//dayone/country/"+Slug;
+  var index = document.getElementById("Slug").selectedIndex;
+  var Slug = document.getElementsByTagName("option")[index].value;
+  console.log(Slug)
+  var apiURL= "https://api.covid19api.com//dayone/country/"+ Slug;
     $.getJSON(apiURL, function (data) {
       console.log(data);
        Country = [];
@@ -104,4 +69,3 @@ Slug: "united-states"
 TotalConfirmed: 1746019
 TotalDeaths: 102809
 TotalRecovered: 406446 */
->>>>>>> master
